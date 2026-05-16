8a59fb6152acfebd67beb60f3047f32c381fe1e5
Nicolas Juan <nicolasjuanllibrer@gmail.com>
2026-05-16 18:11:39 +0200

    Add Next.js config for static export to GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/my_demo__up_restaurant',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
